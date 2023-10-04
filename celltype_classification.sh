9cf7493f4a721c74a7b09a4ea87e9ddc818b0b41
Fabio Azzarello <fabio.azzarello@sns.it>
2023-10-04 10:55:56 +0200

    bash

--- celltype_classification.sh
+++ celltype_classification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=40
-#PBS -q q02daneel
+#PBS -q q02hal
 source activate /home/fazzarello/miniforge3/envs/celltype_classification
 python celltype_classification.py
 
