32f658ae53750adfb213a4cdcf29266d9b1b42c8
Fabio Azzarello <fabio.azzarello@sns.it>
2023-10-05 09:32:49 +0200

    10fold

--- celltype_classification.sh
+++ celltype_classification.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=40
+#PBS -l select=1:ncpus=60
 #PBS -q q02hal
 source activate /home/fazzarello/miniforge3/envs/celltype_classification
 cd /home/fazzarello/CellTypeClassification
