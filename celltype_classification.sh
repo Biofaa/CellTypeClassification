c48a7ac427a01d230e9efacd518ecbc05c1e48f1
Fabio Azzarello <fabio.azzarello@sns.it>
2023-10-24 16:53:02 +0200

    threshold featureselction 0.001

--- celltype_classification.sh
+++ celltype_classification.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=20
+#PBS -l select=1:ncpus=80
 #PBS -q q02hal
 source activate /home/fazzarello/miniforge3/envs/celltype_classification
 cd /home/fazzarello/CellTypeClassification
