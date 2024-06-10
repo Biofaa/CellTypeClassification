e7e33e5af32b486c90c37a35e82792390a151fd3
Fabio Azzarello <fazzarello@trantor01.hpc.sns.it>
2024-06-10 09:44:04 +0200

    new optuna hyperparams

--- celltype_classification.sh
+++ celltype_classification.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#PBS -l select=1:ncpus=80
+#PBS -l select=1:ncpus=56
 #PBS -q q02hal
 source activate /home/fazzarello/miniforge3/envs/celltype_classification
 cd /home/fazzarello/CellTypeClassification
